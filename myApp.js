200cee86c2fdb3a593a8a272635eecf5c9da6078
tira_menali <77180304+soulmedicinas@users.noreply.github.com>
2025-03-31 08:03:22 -0700

    modify queryChain

--- myApp.js
+++ myApp.js
@@ -101,8 +101,14 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find({favoriteFoods:foodToSearch})
+    .sort({name:1})
+    .limit(2)
+    .select({age:0})
+    .exec(function(err, people) {
+      if(err) return done(err);
+      done(null, data)
+    })
 };
 
 /** **Well Done !!**
